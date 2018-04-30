fedbf58ddec2f37b967c026142dbe6891bed4571
Chris Larsen <chris.larsen@qlik.com>
2018-04-30 19:05:55 -0400

    Add files via upload

---
+++ Orchestrator.js
@@ -0,0 +1,122 @@
+/**
+ * HyperQube is an API that seeks to solve low latency challenges using Qlik Sense.
+ * Example API-driven architecture to trigger data updating in ways that are:
+ *	 a. event-driven
+ *	 b. push" style
+ *	 c. variable window size
+ *	 d. incremental/partial 
+ *	 e. orchestrated programmatically
+ * Overall benefit, keeps smaller sized apps working together
+ */
+var app = require('express')();
+var http = require('http').Server(app);
+var noCache = require('connect-nocache')();
+var io = require('socket.io')(http);
+var ioclient = require('socket.io-client')('http://localhost:33333');
+var socketio_port = '33333';
+const Promise = require('bluebird');
+const qsocks = require('qsocks');
+var generateFg=1;
+var sensordata={};
+var windowActive=1;
+var eventArray=[];
+var objectStepComplCount=16; //When to trigger the reload on the child
+var ParentAppReloadCount=objectStepComplCount*4; //when to trigger the reload on the parent
+var triggerreload=0;
+var overrideTriggerReload=0;
+var autoQueue=1;
+var invlType='ObjectComplete';
+var reloadGroup='';
+var reloadId=0;
+var groupReloadId=0;
+var parentApp='';
+var childParentApps=[];
+var appData={};
+var columnLabels=[];
+var columns = [];
+var finalAppData;
+var column={};
+var lastAggReadId_preload=0;
+var unique_readid;
+var sendData=1;
+var messagebroker = 'socket.io'; //kafka'; //or 'socket.io'
+var kafka_ip='153.92.35.73';
+var kafka_port='2181';
+var kafka_topic='iot3';
+
+//Socket.io block start 1
+io.on('connection', function(socket){
+  console.log('got a connection')
+  var appname;
+
+	socket.emit('syncEventArray', eventArray);
+
+  socket.on('generateDataYN', function(data) {
+    generateFg=data.generateFg;
+    console.log('got a generate start/stop request',generateFg)
+    socket.broadcast.emit('generateDataYN', generateFg);
+  });
+
+  var triggerFg=0;
+
+  socket.on('triggerReload', function(data) {
+    triggerFg=data.triggerFg;
+    console.log('got a trigger reload request',triggerFg)
+
+    qsocks.Connect().then(global => {
+       return global.getDefaultAppFolder()
+    })
+    //.then(folder => encodeURIComponent('\\') )
+    .then(folder => {
+      console.log('folder: ',folder)
+      return qsocks.Connect({appname: 'LambdaWindow1.qvf'})//'LambdaWindow1.qvf'})//folder + childParentApps[0].childApps[0]})
+      .then(global => {
+          console.log('got into global')
+          return global.openDoc('LambdaWindow1.qvf').then(app => app, err => {
+              if( err.code === 1002 ) return global.getActiveDoc();
+          })
+      })
+      .then(app => {
+        console.log('trying first reload');
+        
+        //console.log(app)
+        return app.doReload(0, true, false).then(
+            () => app.doSave()
+        )
+        
+      })
+    })
+    .catch(err => console.log('first reload:'+ err))
+
+    socket.broadcast.emit('cleardata',eventArray)
+
+  });
+  
+	socket.on('sensoremit', function(data, callback) {
+    console.log('got a message, arrayLen:',eventArray.length)
+    sensordata=JSON.parse(data);
+		socket.broadcast.emit('bounceemit',sensordata);
+    eventArray.push(sensordata);
+	});
+});
+
+//Express Web Endpoints / REST API's
+http.listen(socketio_port, function(){
+  console.log('listening on *:'+socketio_port);
+});
+
+app.get('/lastsensordata', function (req, res) {
+  if (autoQueue === 1) {
+    var sensordata_string=JSON.stringify(eventArray);
+  } else {
+    var sensordata_string=JSON.stringify(sensordata);
+  }
+  res.end(sensordata_string);
+  setTimeout(clearArray, 2000);
+  function clearArray() {
+    eventArray=[];
+    console.log('Array Cleared');
+
+  }
+  
+});
